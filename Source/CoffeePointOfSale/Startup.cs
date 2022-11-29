48226c0049ff8dd2f1a32abe6a8a4487f13767bb
Tagan Williamson <taganwilliamson@icloud.com>
2022-11-29 16:30:52 -0500

    added csv extract functionality to management screen

--- Source/CoffeePointOfSale/Startup.cs
+++ Source/CoffeePointOfSale/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using CoffeePointOfSale.Services.Drink;
+using CoffeePointOfSale.Services.CsvExtract;
 
 namespace CoffeePointOfSale;
 
@@ -37,6 +38,7 @@ internal static class Startup
         services.AddSingleton<IStorageService, StorageService>();
         services.AddSingleton<ICustomerService, CustomerService>();
         services.AddSingleton<DrinkMenuService>();
+        services.AddSingleton<ICsvExtract, CsvExtract>();
         RegisterForms(services);
         services.AddLogging(configure => configure.AddConsole());
     }
